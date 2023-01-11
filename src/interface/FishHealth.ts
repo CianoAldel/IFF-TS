20886996bfa555978a477e037d8d4b74e1e6e8d9
Khamsing wongupra <cianoaldel@gmail.com>
2023-01-11 11:25:56 +0700

    interface FishHealth

---
+++ src/interface/FishHealth.ts
@@ -0,0 +1,10 @@
+export type FishHealthType = {
+  id: number;
+  product_id: string;
+  user_id: number;
+  symptom: number;
+  status_health: string;
+  status: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
